55fddc7950fe2da128107a4eef8ac4eabf095e24
wangkj <wangkaijie5@jd.com>
2020-04-24 18:31:43 +0800

    add create user

--- login/main.py
+++ login/main.py
@@ -1,22 +1,19 @@
-# 导入数据库模块
+from flask import Flask, render_template, request
 import pymysql
-# 导入Flask框架，这个框架可以快捷地实现了一个WSGI应用
-from flask import Flask
-# 默认情况下，flask在程序文件夹中的templates子文件夹中寻找模块
-from flask import render_template
-# 导入前台请求的request模块
-from flask import request
 import traceback
 # 传递根目录
 app = Flask(__name__)
 
 db_config = {
     'name': 'root',
-    'password': 'wq29635973',
+    'password': '123456',
     'db': 'test_python'
 }
 
-
+'''
+我是
+多行注释
+'''
 # 默认路径访问登录页面
 @app.route('/')
 def login():
@@ -28,6 +25,22 @@ def regist():
     return render_template('regist.html')
 
     # 获取登录参数及处理
+@app.route('/user', methods=['post'])
+def createUser():
+    conn = pymysql.connect("localhost", db_config['name'], db_config['password'], db_config['db'])
+    cursor = conn.cursor()
+    user = str(request.json['name'])
+    password = str(request.json['password'])
+    sql = "INSERT INTO user(user, password) VALUES ('%s','%s')" % (user, password)
+    try:
+        cursor.execute(sql)
+        conn.commit()
+        return 'ok'
+    except: 
+        traceback.print_exc()
+        conn.rollback()
+        return 'fail'
+
 @app.route('/registuser')
 def getRigistRequest():
     #把用户名和密码注册到数据库中
@@ -35,6 +48,7 @@ def getRigistRequest():
     db = pymysql.connect("localhost", db_config['name'], db_config['password'], db_config['db'])
     # 使用cursor()方法获取操作游标 
     cursor = db.cursor()
+    cursor.execute('select * from user where user=' + request.json['name'] + ' and password=' + request.json['password'])
     # SQL 插入语句
     sql = "INSERT INTO user(user, password) VALUES ("+request.args.get('user')+", "+request.args.get('password')+")"
     try:
@@ -53,20 +67,18 @@ def getRigistRequest():
     # 关闭数据库连接
     db.close()
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def getLoginRequest():
-    # 查询用户名及密码是否匹配及存在
-    # 连接数据库,此前在数据库中创建数据库TESTDB
-    db = pymysql.connect("localhost", db_config['name'], db_config['password'], db_config['db'])
-    # 使用cursor()方法获取操作游标
-    cursor = db.cursor()
-    # SQL 查询语句
-    sql = "select * from user where user=" + \
-        request.args.get('user')+" and password=" + \
-        request.args.get('password')+""
     try:
+        connection = pymysql.connect("localhost", db_config['name'], db_config['password'], db_config['db'])
+        # 使用cursor()方法获取操作游标
+        cursor = connection.cursor()
+        # SQL 查询语句
+        sql = 'select * from user where user=' + request.json['name'] + ' and password=' + request.json['password']
         # 执行sql语句
+        # return '1用户名或密码不正确'
         cursor.execute(sql)
+        
         results = cursor.fetchall()
         print(len(results))
         if len(results) == 1:
@@ -74,13 +86,13 @@ def getLoginRequest():
         else:
             return '用户名或密码不正确'
         # 提交到数据库执行
-        db.commit()
+        connection.commit()
     except:
         # 如果发生错误则回滚
         traceback.print_exc()
-        db.rollback()
+        connection.rollback()
     # 关闭数据库连接
-    db.close()
+    connection.close()
 
 
 # 使用__name__ == '__main__'是 Python 的惯用法，确保直接执行此脚本时才
